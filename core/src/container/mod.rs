f02eda74b04c9400347ffe2abf94a1732f5ae496
shshemi <shshemi@gmail.com>
2025-09-03 19:53:39 +0300

    add regex container

--- core/src/container/mod.rs
+++ core/src/container/mod.rs
@@ -3,9 +3,11 @@ mod arc_str;
 mod buffer;
 mod date_time;
 mod line_index;
+mod regex;
 mod traits;
 
 pub use arc_slice::ArcSlice;
 pub use arc_str::ArcStr;
 pub use buffer::{Buffer, LineIter};
 pub use date_time::DateTime;
+pub use regex::Regex;
---
+++ core/src/container/regex.rs
@@ -0,0 +1,32 @@
+pub struct Regex {
+    inner: regex::Regex,
+}
+
+impl Regex {
+    pub fn new(re: &str) -> Result<Self, regex::Error> {
+        Ok(Self {
+            inner: regex::Regex::new(re)?,
+        })
+    }
+    pub fn into_inner(self) -> regex::Regex {
+        self.inner
+    }
+}
+impl<T> From<T> for Regex
+where
+    T: Into<regex::Regex>,
+{
+    fn from(value: T) -> Self {
+        Self {
+            inner: value.into(),
+        }
+    }
+}
+
+impl std::ops::Deref for Regex {
+    type Target = regex::Regex;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
